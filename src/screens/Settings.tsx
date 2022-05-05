1a69f7fcdbedba9e20f67f2d589d6392bf023dbd
Sebastian Pietras <spietras1998@gmail.com>
2022-05-05 13:08:38 +0200

    Fixed orientation changing (#5)

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,15 +1,24 @@
 import tw from "../tailwind";
 import ReactNative from "react-native";
 import { SettingsProps } from "../navigation/types";
-import React from "react";
+import React, { useEffect } from "react";
 import Heading from "../components/text/Heading";
+import { changeScreenOrientation, Orientation } from "../utils";
 
 export default function Settings({ route, navigation }: SettingsProps) {
   const { code, users } = route.params;
 
+  useEffect(() => {
+    navigation.addListener("focus", () => {
+      changeScreenOrientation(Orientation.DEFAULT).then().catch(console.log);
+    });
+  }, [navigation]);
+
   return (
     <ReactNative.View style={tw.style("flex-1", "bg-white")}>
-      <Heading>{code.toString()}</Heading>
+      <ReactNative.View style={tw.style("m-4")}>
+        <Heading>{code.toString()}</Heading>
+      </ReactNative.View>
     </ReactNative.View>
   );
 }
