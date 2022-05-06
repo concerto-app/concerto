a58bd9e8a08c69b2bccbd2b1ee3bb90f8c329c8c
Sebastian Pietras <spietras1998@gmail.com>
2022-05-06 22:08:24 +0200

    Added emoji svg caching (#7)

--- src/components/emojis/Emoji.tsx
+++ src/components/emojis/Emoji.tsx
@@ -1,17 +1,30 @@
 import React from "react";
 import Svg, { SvgProps } from "../Svg";
 import { useSVG } from "@shopify/react-native-skia";
+import useNetworkAsset from "../../hooks/useNetworkAsset";
 
 export type EmojiProps = Omit<SvgProps, "width" | "height" | "svg"> & {
   id: EmojiId;
   size: number;
 };
 
-const idToUri: (code: string) => string = (code: string) =>
+const idToUrl: (code: string) => string = (code: string) =>
   `https://cdn.jsdelivr.net/gh/googlefonts/noto-emoji@v2.034/svg/emoji_u${code}.svg`;
 
-export default function Emoji({ id, size, ...otherProps }: EmojiProps) {
-  const uri = idToUri(id);
+function PrefetchSvg({
+  uri,
+  ...otherProps
+}: Omit<SvgProps, "svg"> & { uri: string }) {
   const svg = useSVG(uri);
-  return <Svg width={size} height={size} svg={svg} {...otherProps} />;
+  return <Svg svg={svg} {...otherProps} />;
+}
+
+export default function Emoji({ id, size, ...otherProps }: EmojiProps) {
+  const url = idToUrl(id);
+  const fileUri = useNetworkAsset(url);
+  return fileUri === null ? (
+    <Svg width={size} height={size} svg={null} {...otherProps} />
+  ) : (
+    <PrefetchSvg width={size} height={size} uri={fileUri} {...otherProps} />
+  );
 }
