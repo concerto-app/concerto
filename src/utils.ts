9db02b3f1df51e9e1220b7a86de9d9762b220246
Sebastian Pietras <spietras1998@gmail.com>
2022-05-06 23:25:03 +0200

    Fixed screen orientation

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,3 @@
-import * as ScreenOrientation from "expo-screen-orientation";
-
-export const Orientation = ScreenOrientation.OrientationLock;
-
-export async function changeScreenOrientation(
-  lock: ScreenOrientation.OrientationLock
-) {
-  await ScreenOrientation.lockAsync(lock);
-}
-
 export function objectMap<A, B>(
   obj: { [s: string]: A },
   fn: (v: A, k: string, i: number) => B
