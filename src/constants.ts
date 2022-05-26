aa799a284360a53a275b3e6f33662dd61d00af9b
Sebastian Pietras <spietras1998@gmail.com>
2022-05-26 20:53:30 +0200

    Adjusted release timings

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,12 @@
 import Constants from "expo-constants";
 import { Nunito_700Bold, Nunito_800ExtraBold } from "@expo-google-fonts/nunito";
 
+export const timing = {
+  releaseAnimationDuration: 200,
+  decayDelay: 500,
+  decayDuration: 500,
+};
+
 export const codeLength = 3;
 
 export const fonts = {
